673f3f6ab4b4c8d511589c7d0df78b5bf0d0bba4
priyamuskan-2901 <muskan.priya.civ21@itbhu.ac.in>
2024-07-06 17:03:00 +0530

    serch bar login signup changed

--- src/component/Layouts/Header1.jsx/Header.jsx
+++ src/component/Layouts/Header1.jsx/Header.jsx
@@ -8,192 +8,6 @@ import { toast } from "react-toastify";
 import { NavLink } from 'react-router-dom';
 // import { useSelector } from "react-redux";
 import { useSelector } from "react-redux";
-// function Header2() {
-//   const navigate = useNavigate();
-//   const { isAuthenticated, user } = useSelector((state) => state.userData);
-
-//   const [searchBarActive, setSearchBarActive] = useState(false);
-
-//   // const [country, setCountry] = useState("in"); // this is for flag
-//   const [sideMenu, setSideMenu] = useState(false);
-//   const [searchValue, setSearchValue] = useState("");
-
-//   // this is for handle sideBar
-//   const handleSideBarMenu = () => {
-//     setSideMenu(!sideMenu);
-//   };
-
-
-
-//   // this is for Search Button toggle
-//   const handleSearchButtonClick = () => {
-//     setSearchBarActive(!searchBarActive);
-//   };
-
-//   // this is for input value of Search bar.
-//   const handleSearchInputChange = (event) => {
-//     setSearchValue(event.target.value);
-//   };
-
-//   // this is for handle searching ...
-//   const handleSearchFormSubmit = (event) => {
-//     event.preventDefault();
-//     if (searchValue.trim()) {
-//       navigate(`/products/${searchValue}`);
-//     } else {
-//       navigate("/products");
-//     }
-//   };
-
-//   // this is for sideBar Toggle Button
-//   const handleCrossButtonClick = () => {
-//     setSearchValue("");
-//     setSearchBarActive(!searchBarActive);
-//   };
-
-//   return (
-//     <>
-//       <div classNameName="header">
-//         <div classNameName="red">
-//           Free delivery on orders above 499
-//         </div>
-
-//         <div classNameName="kriptees-main">
-//           Kriptees
-//           {/* <img
-//                 src={require("../../../Image/logo.png")}
-//                 alt="logo"
-//                 classNameName="headerBottom__logo_main"
-//               /> */}
-//         </div>
-
-//         <div classNameName="headerBottom">
-
-//           <div classNameName="headerBottom__logo">
-//             <div classNameName="header_mobile_menu">
-//               <span>
-//                 <ReorderIcon
-//                   onClick={() => setSideMenu(!sideMenu)}
-//                   sx={{
-//                     fontSize: 29,
-//                     color: "black",
-//                     "&:hover": {
-//                       color: "#e7070f",
-//                       cursor: "pointer",
-//                     },
-//                   }}
-//                 />
-//                 {sideMenu && (
-//                   <Sidebar
-//                     handleSideBarMenu={handleSideBarMenu}
-//                     isAuthenticated={isAuthenticated}
-//                     user={user}
-//                   />
-//                 )}
-//               </span>
-
-//               <div classNameName="kriptees-mobile">
-//                 Kriptees
-//                 {/* <img
-//                 src={require("../../../Image/logo.png")}
-//                 alt="logo"
-//                 classNameName="headerBottom__logo_main"
-//               /> */}
-//               </div>
-//               {/* <span>
-//                 <SearchBar
-//                   searchBarActive={searchBarActive}
-//                   searchValue={searchValue}
-//                   handleCrossButtonClick={handleCrossButtonClick}
-//                   handleSearchButtonClick={handleSearchButtonClick}
-//                   handleSearchInputChange={handleSearchInputChange}
-//                   handleSearchFormSubmit={handleSearchFormSubmit}
-//                 />
-//               </span> */}
-//             </div>
-//           </div>
-
-//           {/* navmenu */}
-
-//           {!searchBarActive && (
-//             <div classNameName="headerBottom_navMenu">
-//               <ul>
-//                 <li>
-//                   <Link to="/">Home</Link>
-//                 </li>
-
-//                 <li>
-//                   <Link to="/products">Product</Link>
-//                 </li>
-//                 <li>
-//                   <Link to="/contact">Contact</Link>
-//                 </li>
-//                 <li>
-//                   <Link to="/about_us">About</Link>
-//                 </li>
-//               </ul>
-//             </div>
-//           )}
-
-//           {/* icons */}
-
-//           <div classNameName="headerBotttom_icons">
-//             <div classNameName="search_Bar">
-//               <SearchBar
-//                 searchBarActive={searchBarActive}
-//                 searchValue={searchValue}
-//                 handleCrossButtonClick={handleCrossButtonClick}
-//                 handleSearchButtonClick={handleSearchButtonClick}
-//                 handleSearchInputChange={handleSearchInputChange}
-//                 handleSearchFormSubmit={handleSearchFormSubmit}
-//               />
-//             </div>
-//             <div>
-//               <Link
-//                 to="/cart"
-//                 style={{ color: "none", textDecoration: "none" }}
-//               >
-//                 <IoBagOutline />
-
-//               </Link>
-//             </div>
-//             <div>
-//               <ProfileModal user={user} isAuthenticated={isAuthenticated} />
-//             </div>
-
-//             <div classNameName="headerLogin">
-//               {isAuthenticated ? (
-//                 <Link
-//                   to="/account"
-//                 // style={{ color: "inherit", textDecoration: "none" }}
-//                 >
-//                   <button>My Account</button>
-//                 </Link>
-//               ) : (
-//                 <div>
-//                   < Link to="/login">
-//                     <button>
-//                       Login
-//                     </button>
-//                   </Link>
-
-//                   <Link
-//                     to="/signup"
-//                   // style={{ color: "inherit", textDecoration: "none" }}
-//                   >
-//                     <button>Sign Up</button>
-//                   </Link>
-//                 </div>
-//               )}
-//             </div>
-//           </div>
-
-
-//         </div>
-//       </div >
-//     </>
-//   );
-// }
 
 
 function Header() {
@@ -287,30 +101,20 @@ function Header() {
       </div>
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
-        <div className="flex items-center justify-center h-full">
-          <Link to={'/'}>
+      <div className="flex items-center justify-center h-full">
+          <Link to={"/"}>
             <span className="kriptees-main">Kriptees</span>
           </Link>
         </div>
 
-
-
-        <div className="flex items-center md:order-2 space-x-3  rtl:space-x-reverse">
-
-          <form className=" lg:max-w-md mx-auto">
-            <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-            <div className="relative">
-              <button type="submit" className="lg:absolute inset-y-0 start-0 flex items-center ps-3 ">
-                <svg className="lg:w-4 lg:h-4 w-5 h-5 text-gray-700 lg:text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                </svg>
-              </button>
-              <input type="search" id="default-search" className=" hidden lg:block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
-            </div>
-          </form>
+        <div className="flex items-center md:order-2 space-x-3 rtl:space-x-reverse">
+          <div onClick={handleSearchButtonClick} className="flex hover:cursor-pointer justify-center items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-4.35-4.35m0 0a7.5 7.5 0 111.415-1.415l4.35 4.35zM12 19.5a7.5 7.5 0 100-15 7.5 7.5 0 000 15z" />
+            </svg>
+          </div>
           
-          {/* changed here for the cart red notifiaction */}
-          <div onClick={() => { navigate("/cart") }} className="flex hover:cursor-pointer justify-center items-center">
+          <div onClick={cartHandler} className="flex hover:cursor-pointer justify-center items-center">
             <div className="relative">
               {cartItemCount > 0 && (
                 <div className="-top-4 absolute left-3">
@@ -325,38 +129,26 @@ function Header() {
             </div>
           </div>
 
-
-          <div className=" hidden lg:flex">
+          <div className="hidden lg:flex">
             {isAuthenticated ? (
-              <button onClick={() => {
-                setInfo(!info)
-              }} type="button" className="flex text-smrounded-full justify-center items-center md:me-0 " id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+              <button onClick={() => setInfo(!info)} type="button" className="flex text-sm rounded-full justify-center items-center md:me-0" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                 <span className="sr-only">Open user menu</span>
                 <img className="w-8 h-8 rounded-full" src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="user photo" />
-                <div className=" max-w-sm">{user.name}</div>
+                <div className="max-w-sm">{user.name}</div>
               </button>
             ) : (
-              <div className=" flex">
-                < Link to="/login">
-                    <div className="m-3 bg-blue-700 text-white py-2 px-4 cursor-pointer hover:bg-blue-800" style={{ borderRadius: '12px' }}>
-                      Login
-                    </div>
-
-
+              <div className="flex">
+                <Link to="/login">
+                  <div className="m-3 bg-blue-700 text-white py-2 px-4 cursor-pointer hover:bg-blue-800" style={{ borderRadius: '12px' }}>
+                    Login
+                  </div>
                 </Link>
-
-                  <Link to="/signup">
-                  
-                
-                    <div className="m-3 text-white py-2 px-4 cursor-pointer" style={{ backgroundColor: '#DA0440', borderRadius: '12px' }}>
-                      Sign Up
-                    </div>
-
-
+                <Link to="/signup">
+                  <div className="m-3 text-white py-2 px-4 cursor-pointer" style={{ backgroundColor: '#DA0440', borderRadius: '12px' }}>
+                    Sign Up
+                  </div>
                 </Link>
-
               </div>
-
             )}
           </div>
 
@@ -525,6 +317,28 @@ function Header() {
         )}
 
       </div>
+      {searchBarActive && (
+        <div className="bg-white dark:bg-gray-900 absolute top-0 left-0 right-0 w-full h-full z-50 flex items-center justify-center">
+          <form onSubmit={handleSearchFormSubmit} className="w-full max-w-2xl mx-auto p-4 flex items-center">
+            <input
+              type="text"
+              value={searchValue}
+              onChange={handleSearchInputChange}
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg dark:bg-gray-700 dark:text-white"
+              placeholder="Search for products..."
+              autoFocus
+            />
+            <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">
+              Search
+            </button>
+            <button type="button" onClick={handleCrossButtonClick} className="ml-2 text-gray-500 dark:text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </form>
+        </div>
+      )}
     </nav>
   )
 }
